e513d7d730adcbe9a3e665069d32e9fb350841db
amic2264@gmail.com <ec2-user@ip-172-31-13-45.ec2.internal>
2021-08-15 06:35:17 +0000

    [add]homes top route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
+  root to: 'homes#top'
   namespace :member do
-    get 'shipping/products'
+
   end
-  namespace :member do
+  namespace :owner do
     get 'shipping/addresses'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
