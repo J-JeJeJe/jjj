6a9d5de5bd2ebd6fee5630b52172a74d9a95db41
laoooba <yama10taku31@gmail.com>
2021-08-16 07:49:19 +0000

    finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,16 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
-    
+
   def after_sign_in_path_for(resource)
+    if member_signed_in?
+      root_path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
     root_path
   end
 
