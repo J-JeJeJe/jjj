a496abdc08aec7eba338af8f0bacee3ddd6d99be
Megumu-Suzuki <m.suzuki.te@gmail.com>
2021-08-16 16:28:39 +0000

    [Fix]date

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,22 @@
 class Public::OrdersController < ApplicationController
+  
+  def new
+    @order = Order.new
+    @shipping_addresses = ShippingAddress.where(public_id: current_member)
+  end
+
+  def confirm
+  end
+
+  def create
+  end
+
+  def thanx
+  end
+
+  def index
+  end
+
+  def show
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  
+  enum order_status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み:4}
+ 
+  #消費税を加えた金額
+  def add_tax_price
+        (product.price * 1.10).round
+  end
 end
