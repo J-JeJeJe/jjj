146abba598e56f733157acb5814c13e1c755fd16
laoooba <yama10taku31@gmail.com>
2021-08-16 01:39:15 +0000

    add  fix owners

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   
   devise_for :members
-  devise_for :owners
+  
   root to: 'homes#top'
   namespace :member do
 
   end
   namespace :owner do
     get 'shipping/addresses'
+    devise_for :owners
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
