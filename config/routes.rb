f011d7c614c3a6b25f36886568f2501f6116fec1
laoooba <yama10taku31@gmail.com>
2021-08-17 01:55:14 +0000

    [add] routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # public-start-
+  scope module: :public do
+    resources :products, only:[:index]
+  end
+  # public-finish-
+  
+  
+  
+  # admin-start-
+  namespace :admin do
+  end
+  # admin-finish
 end
