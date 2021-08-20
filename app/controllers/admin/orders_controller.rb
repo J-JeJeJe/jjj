ca407d71a47a6f5de7deab1f39ed095bcbbb7e01
laoooba <yama10taku31@gmail.com>
2021-08-20 10:21:55 +0000

    [add] index show to page

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,12 +1,13 @@
 class Admin::OrdersController < ApplicationController
 
+
   def index
     @orders = Order.page(params[:page]).per(10)
   end
 
   def show
-    @orders = Order.all
     @order = Order.find(params[:id])
+    @order_items = OrderItem.where(order_id: params[:id])
   end
 
   def update
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,19 @@ class OrderItem < ApplicationRecord
 
   belongs_to :order
   belongs_to :product
+  
+  
+  
+  #消費税を加えた金額
+  def add_tax_price
+    (self.product.price * 1.10).round
+  end
+  # 小計
+  def sub_total
+    self.add_tax_price * self.quantity
+  end
+ 
+  
+  enum work_status: { 着手不可: 0, 制作待ち: 1, 製作中: 2, 制作完了: 3}
 
 end
