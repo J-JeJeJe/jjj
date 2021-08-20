f40547a131afb78e25c46fd1e789ad8b21fc8fe2
Megumu-Suzuki <m.suzuki.te@gmail.com>
2021-08-20 10:46:58 +0000

    [Add]product-category function

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,13 +5,15 @@ class Admin::ProductsController < ApplicationController
     end
 
     def create
-        product = Product.new(product_params)
-        product.save
-        redirect_to admin_product_path(product.id)
+        @product = Product.new(product_params)
+        if @product.save
+        redirect_to admin_product_path(@product.id)
+        else
+        render :new
+        end
     end
 
     def index
-        @products = Product.all
         @products = Product.page(params[:page]).per(10)
     end
 
@@ -24,16 +26,17 @@ class Admin::ProductsController < ApplicationController
     end
 
     def update
-        product = Product.find(params[:id])
-        product.update(product_params)
-        redirect_to admin_product_path(product)
+        @product = Product.find(params[:id])
+        @product.update(product_params)
+        logger.debug @product.errors.inspect
+        redirect_to admin_product_path(@product.id)
     end
 
 
     private
 
     def product_params
-        params.require(:product).permit(:image, :title, :description, :price, :sell_status, :product_cate)
+        params.require(:product).permit(:image, :title, :description, :price, :sell_status, :product_cate, :product_category_id)
     end
 
 end
