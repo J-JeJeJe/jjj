f396adda3e04e97a5034b493aa9fcf07e920bfb3
laoooba <yama10taku31@gmail.com>
2021-08-21 07:43:07 +0000

    [finish]

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,15 @@
 class Admin::OrderItemsController < ApplicationController
+
+  def update
+    @order_item = OrderItem.find(params[:id])
+    if @order_item.update(order_item_params)
+      @order = Order.find(params[:id])
+      redirect_back(fallback_location: admin_order_path(@order))
+    end
+  end
+
+  private
+  def order_item_params
+    params.require(:order_item).permit(:work_status)
+  end
 end
