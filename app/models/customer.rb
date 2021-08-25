9fd948a15c0def19678ef12b0810d5c944174621
laoooba <yama10taku31@gmail.com>
2021-08-25 10:40:33 +0000

    finish

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,15 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, presence: true
+  validates :postal_code, presence: true, numericality: {only_integer: true},
+                          length: { is: 7 }
+  validates :address, presence: true,
+                      length: { maximum: 100 }
+  validates :phone_number, presence: true, numericality: {only_integer: true},
+                          length: { minimum: 10, maximum: 11 }
+  
+  
     #注文機能
     has_many :orders, dependent: :destroy
 
